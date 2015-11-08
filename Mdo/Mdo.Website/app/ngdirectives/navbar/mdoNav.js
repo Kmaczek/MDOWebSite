c867c5ef061405c259db1ffaf1131ef4ef7e56f2
Damian Kmak <flosbox@gmail.com>
2015-11-08 13:38:13 +0100

    moved navbar to directive, css for login inputs

---
+++ Mdo/Mdo.Website/app/ngdirectives/navbar/mdoNav.js
@@ -0,0 +1,24 @@
+﻿(function () {
+
+    var link = function(scope, element, attrs) {
+        scope.login = login;
+
+        function login() {
+            console.log(scope.username);
+            console.log(scope.password);
+        }
+    }
+
+    var mdoNav = function() {
+        return {
+            templateUrl: 'app/ngdirectives/navbar/mdoNav.template.html',
+            scope: {
+                login: '='
+            },
+            link: link
+        }
+    }
+
+    angular.module('mdo').directive('mdoNav', mdoNav);
+}());
+
